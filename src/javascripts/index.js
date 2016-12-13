f88de4c9e15d002ae11ce77c7164f15dea57df00
Daniel Ribeiro <djbreves@gmail.com>
2016-12-13 14:45:19 +1100

    Don't resize nav bar app

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -6,14 +6,11 @@ var client = ZAFClient.init();
 window.zafClient = client;
 
 client.on('app.registered', function(data) {
-  var height = '100%',
-      location = data.context.location;
+  var location = data.context.location;
 
   new App(client, data);
 
   if (location === 'top_bar' || /_sidebar$/.test(location)) {
-    height = '500px';
+    client.invoke('resize', { height: '500px' });
   }
-
-  client.invoke('resize', { height: height });
 });
